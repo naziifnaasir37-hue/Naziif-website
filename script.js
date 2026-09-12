a335df05b19dd90b8bfde7eb69a9668918a541d3
Naziif <Naziifnaasir37@gmail.com>
2026-09-12 20:39:19 +0300

    Update personal website

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mobile menu
+function showMessage() {
+    alert("Welcome to Naziif Website! 🚀");
+}
+
+// Dark / Light Mode
+function toggleTheme() {
+    document.body.classList.toggle("light-mode");
+}
